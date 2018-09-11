47a667a60b101dae4a015b12fe8862f0807cf85c
Tomas Carnecky <tomas.carnecky@gmail.com>
2018-09-11 19:33:57 +0200

    Remove Object.assign polyfill

--- pkg/js/avers/src/shared.ts
+++ pkg/js/avers/src/shared.ts
@@ -3,7 +3,7 @@ export function last<T>(xs: T[]): T {
 }
 
 export function immutableClone<T>(ctor: new (...args: any[]) => T, x: T, f: (x: T) => void): T {
-  let copy = assign(new ctor(), x);
+  const copy = Object.assign(new ctor(), x);
   f(copy);
   return Object.freeze(copy);
 }
@@ -17,43 +17,3 @@ export function guardStatus(functionName: string, ...statuses: number[]): (res: 
     }
   };
 }
-
-// ---
-// Object.assign polyfill
-
-let propIsEnumerable = Object.prototype.propertyIsEnumerable;
-
-function ToObject<T extends Object>(val: any): T {
-  if (val == null) {
-    throw new TypeError("Object.assign cannot be called with null or undefined");
-  } else {
-    return Object(val);
-  }
-}
-
-function ownEnumerableKeys<T>(obj: T): (string | symbol)[] {
-  let keys: (string | symbol)[] = Object.getOwnPropertyNames(obj);
-
-  if (Object.getOwnPropertySymbols) {
-    keys = keys.concat(Object.getOwnPropertySymbols(obj));
-  }
-
-  return keys.filter(key => propIsEnumerable.call(obj, key));
-}
-
-export function assign<T>(target: T, ..._source: Object[]): T {
-  let to = ToObject<T>(target);
-
-  for (let s = 1; s < arguments.length; s++) {
-    let from = arguments[s],
-      keys = ownEnumerableKeys(Object(from));
-
-    for (let i = 0; i < keys.length; i++) {
-      (to as any)[keys[i]] = from[keys[i]];
-    }
-  }
-
-  return to;
-}
-
-// ---
