f43e4dca5216d9ee29389938861a862f4c5a6d82
Tomas Carnecky <tomas.carnecky@gmail.com>
2018-12-15 09:14:55 +0100

    Clear networkRequest on Editable after we receive a response

--- pkg/js/avers/src/storage/api/applyServerResponse.ts
+++ pkg/js/avers/src/storage/api/applyServerResponse.ts
@@ -6,6 +6,8 @@ import { applyPatches } from "../internal/applyPatches";
 
 function applyServerResponseF(h: Handle, { objId, body }: { objId: string; body: any }): void {
   changeEditable(h, objId, obj => {
+    obj.networkRequest = undefined;
+
     // Clear out any traces that we've submitted changes to the
     // server.
     obj.submittedChanges = [];
@@ -18,10 +20,6 @@ function applyServerResponseF(h: Handle, { objId, body }: { objId: string; body:
 const applyServerResponseA = (objId: ObjId, body: any) =>
   mkAction(`applyServerResponse(${objId})`, { objId, body }, applyServerResponseF);
 
-export const applyServerResponse = (
-  h: Handle,
-  objId: ObjId,
-  body: any
-): void => {
+export const applyServerResponse = (h: Handle, objId: ObjId, body: any): void => {
   modifyHandle(h, applyServerResponseA(objId, body));
 };
