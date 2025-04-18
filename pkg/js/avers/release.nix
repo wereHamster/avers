5df25b9150e2c36e42a78ace69050561830a00cd
Tomáš Čarnecký <tomc@caurea.org>
2025-04-18 12:38:01 +0200

    Update canary release script

--- pkg/js/avers/release.nix
+++ pkg/js/avers/release.nix
@@ -8,25 +8,18 @@ in {
 
     buildInputs = [
       pkgs.nodejs
-      pkgs.pnpm
       pkgs.jq
-      pkgs.biome
     ];
 
     buildPhase = ''
-      HOME=$PWD pnpm install --frozen-lockfile
       npm version patch
-      ./node_modules/.bin/tsc
       jq 'del(.devDependencies) | .version = .version + "-alpha.${n}+${commit}"' package.json > temp.json && mv temp.json package.json
     '';
 
-    checkPhase = ''
-      biome lint .
-      ./node_modules/.bin/ava
-    '';
-
     installPhase = ''
-      mv . $out
+      mkdir -p $out
+      cp -rv . $out
+      rm -rf $out/node_modules
     '';
   };
 }
