4e094492de402f2b29eee708ac81351efe656b4e
Tomáš Čarnecký <tomc@caurea.org>
2026-03-21 17:55:38 +0100

    Update Nix flake in pkg/js/avers (#740)

--- pkg/js/avers/flake.nix
+++ pkg/js/avers/flake.nix
@@ -4,22 +4,28 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
 
-        in {
-          devShells.default = pkgs.mkShell {
-            buildInputs = [
-              pkgs.nodejs
-              pkgs.pnpm
-              pkgs.biome
-            ];
-          };
-        }
-      );
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+            pkgs.pnpm
+            pkgs.biome
+          ];
+        };
+      }
+    );
 }
